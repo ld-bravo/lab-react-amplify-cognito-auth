cd45e64f7142ed5c0204b0e05c8c1b70f0e5a927
Lester Bravo <ld.bravo@outlook.com>
2024-10-15 00:45:32 -0600

    Login update

--- src/App.tsx
+++ src/App.tsx
@@ -57,9 +57,6 @@ export default function App() {
         <Route path="*" element={<HomePage isAuthenticated={isAuthenticated} />} />
         <Route path="/" element={<HomePage isAuthenticated={isAuthenticated} />} />
         <Route path="/login" element={<LoginPage isAuthenticated={isAuthenticated} updateAuthStatus={updateAuthStatus} />} />
-        {/* <Route path="/register" element={<RegisterPage />} />
-        <Route path="/validate" element={<ValidatePage />} />
-        <Route path="/contacts" element={<Contacts isAuthenticated={isAuthenticated} />} /> */}
       </Routes>
       {isAuthenticated &&
         <Footer />
